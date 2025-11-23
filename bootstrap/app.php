7f0a33288334c2b3d3527081264acf4a9d7652c8
AdamJardalii <adam.jardali@lau.edu>
2025-11-23 18:18:51 +0200

    fix code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,11 +4,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',   
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -20,5 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (MethodNotAllowedHttpException $e) {
+            return redirect()->route('dashboard')->with('error', 'Invalid request method.');
+        });
     })->create();
